ea9fef5e409ef3cf64f47a7c0d9ed03bea291df3
Nat Kale <nathanielkale@gmail.com>
2013-12-05 16:56:54 -0600

    Multiple site upload working.
    Added new variables to settings.ini & modified script flow to incorporate multiple sites.
    Must manually add benchmarks (or offsets, or zero elevations) to each site.

--- UploadOttData.py
+++ UploadOttData.py
@@ -16,7 +16,7 @@ from datetime import datetime
 # Config file needs to be in the same directory as the script; otherwise, alter configPath to match the location.
 configPath = "ott_settings.ini"
 
-mtypeid     = "ELEV"
+mtypeid     = "LL"
 zeroElev    = 900
 insertSQL   = " INSERT INTO `measurements` (`mtime`, `value`, `siteid`, `mtypeid`, `lab_id`) VALUES ('{0}', {1}, '{2}', '{3}', 'N/A') "
 sensor      = ""
@@ -30,8 +30,8 @@ config = ConfigParser.ConfigParser()
 config.read(configPath)
 
 #Set paths
-downloadDir = config.get('filepaths','download_paths')
-archiveDir = config.get('filepaths','archive_paths')
+downloadDir = config.get('filepaths','download_path')
+archiveDir = config.get('filepaths','archive_path')
 
 # Connect to FTP
 ftp_dir     = config.get('ftp','url')
@@ -48,7 +48,10 @@ mysql_url   = config.get('mysql','url')
 mysql_usr   = config.get('mysql','username')
 mysql_pwd   = config.get('mysql','password')
 mysql_db    = config.get('mysql','database')
-mysql_ids   = config.get('mysql','siteids').split(",")
+
+#Get site variables
+site_ids        = config.get('sites','ids').split(",")
+site_offsets    = config.get('sites','offsets').split(",")
 
 #Open archive
 archive = zipfile.ZipFile(archiveDir, "a")
@@ -56,7 +59,7 @@ archive = zipfile.ZipFile(archiveDir, "a")
 i = 0
 
 #Loop through each site
-for id in mysql_ids:
+for id in site_ids:
 
     numFiles    = 0
     numVals     = 0
@@ -71,14 +74,12 @@ for id in mysql_ids:
     files = ftp.nlst(target_dirs[i])
 
     #Grab the date/time of the most recent measurement recorded.
-    cur.execute("SELECT `mtime` FROM `measurements` WHERE `siteid` = '{}' ORDER BY `mtime` DESC LIMIT 1 ".format(id))
+    cur.execute("SELECT `mtime` FROM `measurements` WHERE `siteid` = '{}' and `mtypeid` = '{}' ORDER BY `mtime` DESC LIMIT 1 ".format(id,mtypeid))
     if cur.rowcount > 0:
         lastMeasureDate = cur.fetchone()[0]
     else:
         lastMeasureDate = datetime.strptime("19900101010000","%Y%m%d%H%M%S")
     logging.info("Most recent data uploaded on: {0}".format(lastMeasureDate))
-
-
 
     # Grab data from files, & import to WQDB
     for file in files:
@@ -88,7 +89,7 @@ for id in mysql_ids:
             fileName = file.split(target_dirs[i])[1]
             logging.info(fileName)
             
-            #Ott log filenames follow a specific format; (padded) site name first, followed by date & time
+            #Ott log filenames follow a specific format; (zero-padded) site name first, followed by date & time
             filedatetime = datetime.strptime(fileName[11:25],"%Y%m%d%H%M%S")
             
             #Read out data in files not yet entered in the DB
@@ -101,22 +102,30 @@ for id in mysql_ids:
                 #Parse data, pulling just the elevation data & inserting into DB
                 if len(lines) > 1:
                     for line in lines:
+                        #If the line is a "station" line, pull the sensor value
                         if line[0] == "<":
                             fixedLine   = "<ln>" + line + "</ln>"
                             values      = xmlParser.fromstring(fixedLine)
                             sensor      = values.find("SENSOR").text
+                        #Sensor 0001 should always be water depth from the zero elevation (offset) in feet.
+                        # Every line between two "station" lines should be a different time reading for the same sensor.
                         elif sensor == "0001":
+                            #Prep the values
                             vals.append(line.strip().split(":"))
                             theDate     = line.strip().split(";")[0]
                             theTime     = line.strip().split(";")[1]
-                            val         = zeroElev + float(line.strip().split(";")[2])
+                            val         = float(site_offsets[i]) + float(line.strip().split(";")[2])
                             dtime       = str(theDate) + str(theTime)
                             thedtime    = datetime.strptime(dtime, "%Y%m%d%H%M%S")
                             timeFormat  = thedtime.strftime("%Y-%m-%d %H:%M:%S")
+                            
+                            #Insert the values in the DB
                             sql         = insertSQL.format(timeFormat, val, id, mtypeid)
                             cur.execute(sql)
                             con.commit()
-                            logging.info(sql)
+                            
+                            #Wrap up - log & increment
+                            #logging.info("  " + sql)
                             numVals     = numVals + 1
                 else:
                     logging.info(file + " has no data.")
@@ -127,13 +136,12 @@ for id in mysql_ids:
                 os.remove(downloadDir + fileName)
                 ftp.delete(file)
 
-    logging.info("Read {0} files from FTP; added {1} values to the database.\n\n".format(numFiles, numVals))
-    
+    logging.info("\nRead {0} files from FTP; added {1} values to the database.".format(numFiles, numVals))
     con.close()
     i+=1
 
-logging.info("\n---\n")
+logging.info("\n\n\n\n")
 
 # Close connections
 ftp.quit()
-
+archive.close()
